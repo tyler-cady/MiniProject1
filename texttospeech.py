e068f102104e7f814a146e846e9bea54e6bde337
Tyler Cady <cadytm@clarkson.edu>
2024-01-16 10:13:57 -0500

    Test app for selfie.py created

--- texttospeech.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-pipe = pipeline("text-to-speech", model="suno/bark-small")
-text = "[clears throat] This is a test ... and I just took a long pause."
-output = pipe(text)
-
-from IPython.display import Audio
-Audio(output["audio"], rate=output["sampling_rate"])
