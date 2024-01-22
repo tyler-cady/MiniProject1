c0cb6787059f4a76eea622ac7fd9d00a6b83eb3e
Tyler Cady <cadytm@clarkson.edu>
2024-01-22 11:34:15 -0500

    start of scene

--- scene.py
+++ scene.py
@@ -1,28 +1,21 @@
-# Selfie app V0.3
-# Current funcionality: take photos & determine where the user's face is
-# Created by: Tyler Cady
+# Static Scene App V 0.1
+# Current funcionality: 
+# Created by: Tyler Cady, 
 # Last Edit: 1.18.24
 import cv2
 # using yolov8 to detect objects in the image
 import pygame
+import time
+from ultralytics import YOLO
 
-
+# Load the model
+model = YOLO('Yolov8n.pt')
 class SceneApp:
+
     def __init__(self):
         # Init camera, face detection, and sound
         self.capture = cv2.VideoCapture(0)
-        self.mp_face_detection = mp.solutions.face_detection
-        self.face_detection = self.mp_face_detection.FaceDetection(
-            min_detection_confidence=0.2)
         pygame.mixer.init()
-
-    def take_photo(self):
-        # Capturen a frame, save photo, play shutter sound
-        ret, frame = self.capture.read()
-        if ret:
-            cv2.imwrite("photo.jpg", frame)
-            print("Photo taken and saved as photo.jpg")
-            self.play_sound("resources/shutter.mp3")
 
     def take_photo(self):
         # Capture a frame, save photo, play shutter sound
@@ -34,6 +27,15 @@ class SceneApp:
             cv2.imwrite(file_name, frame)
             print(f"Photo taken and saved as {file_name}")
             self.play_sound("resources/shutter.mp3")
-if __name__ == "__main__":
+    
+    def play_sound(self, file_path):
+        """
+        Play a sound from a file.
+        """
+        pygame.mixer.music.load(file_path)
+        pygame.mixer.music.play()
+
+
+if__name__ == "__main__":
     app = SceneApp()
     app.run()
